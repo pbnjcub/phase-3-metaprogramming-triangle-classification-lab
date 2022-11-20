620815a680ccf0d6e44de670e316fa750eba1db6
Paul Jung <pbnjcub@gmail.com>
2022-11-20 16:15:12 -0500

    commit tree d048e603ffedc20fd25433a49b68635a2c3f1f04

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
+require 'pry'
+
 class Triangle
   # write code here
+  attr_accessor :side_one, :side_two, :side_three
+
+  @@side_one = " "
+  @@side_two = " "
+  @@side_three = " "
+
+  def initialize(one, two, three)
+    @@side_one = one
+    @@side_two = two
+    @@side_three = three
+  end
+
+  def kind
+    if @@side_one > 0 and @@side_two > 0 and @@side_three > 0
+      if @@side_one + @@side_two <= @@side_three or @@side_one + @@side_three <= @@side_two or @@side_two + @@side_three <= @@side_one
+        begin
+          raise TriangleError
+        end
+      elsif @@side_one == @@side_two and @@side_one == @@side_three
+        :equilateral
+      elsif @@side_one == @@side_two or @@side_one == @@side_three or @@side_two == @@side_three
+        :isosceles
+      else
+        # @@side_one != @@side_two and @@side_one != @@side_three and @@side_two != @@side_three
+        :scalene
+      end
+    else
+      begin
+        raise TriangleError
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
+
+binding.pry
